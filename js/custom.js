d6be353d419b1d0f7ccd4485a2e53a33fc8bacdb
mzsbulbul <mzsbulbul@github.com>
2019-01-26 23:41:51 +0600

    Added Slider on feature part

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ $(function(){
     $('.feature_slider').slick({
       infinite: true,
       autoplay: true,
-      autoplaySpeed: 2000,
+      autoplaySpeed: 2500,
       slidesToShow: 4,
       slidesToScroll: 1,
       arrows: false,
@@ -12,28 +12,25 @@ $(function(){
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToScroll: 1,
             infinite: true,
-            dots: true
+            dots: false,
           }
         },
         {
           breakpoint: 600,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToScroll: 1,
           }
         },
         {
           breakpoint: 480,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
           }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
       ]
     });
     });

793d9d0dd360ecb69bf3cd45044d6f4e08fc30c8
DevZiaus <mzsbulbul@gmail.com>
2024-07-10 09:00:56 +0600

    Added Google Adsense code & Modified some content to reflect MERN & Full Stack developmetn

--- js/myType.js
+++ js/myType.js
@@ -2,7 +2,7 @@ $(function () {
 
 /*** Type Effect ***/
     var typed = new Typed(".my-text", {
-        strings: ["Md Ziaus Samad", "a Front End Web Developer", "a PSD to HTML Expert", "a Wordpress Expert", "a Back End Web Developer", "a Full Stack Web Developer"],
+        strings: ["Md Ziaus Samad", "a Full Stack Developer","a MERN Stack Developer", "a Front End Web Developer", "a PSD to HTML Expert", "a Back End Web Developer", "a Laravel Developer"],
         smartBackspace: true, // Default value
         loop: true,
         backDelay: 1000,
